a74bc7dc74122993f3d1dfc18586f8ee794b69a8
Morksha <rmorksha@gmail.com>
2022-08-11 01:03:26 +0530

    no message

--- nodeapi/app.js
+++ nodeapi/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser')
 const expressValidator = require("express-validator");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -27,9 +28,17 @@ const authRoutes = require("./routes/auth");
 //middleware
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(expressValidator());
 app.use("/", postRoutes);
 app.use("/", authRoutes);
+app.use(function(err, req, res, next) {
+    if (err.name === "UnauthorizedError") {
+        res.status(401).json({ error: "Unauthorized token..." });
+    } else {
+        next(err);
+    }
+});
 
 
 //listen to application 
--- nodeapi/controllers/auth.js
+++ nodeapi/controllers/auth.js
@@ -1,3 +1,6 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+const expressJwt = require("express-jwt");
 const User = require("../models/user");
 
 exports.signup = async(req, res) => {
@@ -9,4 +12,46 @@ exports.signup = async(req, res) => {
     const user = await new User(req.body)
     await user.save()
     res.status(200).json({ message: "Signup success! please login" });
+};
+
+exports.signin = (req, res) => {
+    //find the user based on email
+    const { email, password } = req.body
+    User.findOne({ email }, (err, user) => {
+        //if err or no user
+        if (err || !user) {
+            return res.status(401).json({
+                error: "User with that email does not exist. Please signin."
+            })
+        }
+
+        //if user is found make sure the email and password matches
+        //create authentication
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and password do not match"
+            })
+        }
+
+        //generate a token with user ID and secret 
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+        //persist the token as 't' in cookie with expiry date  
+        res.cookie("t", token, { expire: new Date() + 9999 });
+        //return responses with user and token to frontend client
+        const { _id, name, email } = user
+        return res.json({ token, user: { _id, email, name } });
+
+
+    })
+
 }
+
+exports.signout = (req, res) => {
+    res.clearCookie("t");
+    return res.json({ message: "Sign out successfully!" });
+
+};
+
+// exports.requireSignin = expressJwt({
+//     secret: process.env.JWT_SECRET
+// });
